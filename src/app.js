88d6a5415b8d968b98048314b6d5605742349a02
Hritik Arora <hritikarora875@gmail.com>
2021-06-17 19:52:51 +0530

    3rd commit

--- src/app.js
+++ src/app.js
@@ -1,13 +1,21 @@
 const express = require('express');
+require("./db/conn")
+const Student = require("./models/students")
 const app = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
 
+app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("Your are in root")
-})
+//Create a new student
 app.post("/student",(req,res) =>{
-    res.send("We are in student");
+    console.log(req.body);
+    const user = new Student(req.body);
+    user.save().then(()=>{
+        res.status(201).send(user);
+    }).catch((e)=>{
+        res.status(400).send(e)
+    })
+    
 })
 
 app.listen(port, ()=>{
